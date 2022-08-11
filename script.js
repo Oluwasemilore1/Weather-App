7a449a68359cc5d0e5f32f94a06dc929284aa59f
unknown <oluwasemiloreadetayo@gmail.com>
2022-08-11 11:29:37 +0100

    correction

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 const searchBtn = document.getElementById('Search')
+let temperature = document.getElementById('weather-temp');
+let city = document.getElementById('location')
+let ions = document.getElementById('weather-icon')
+let dayday = document.getElementById('date-name')
+let cweather = document.getElementById('weather-desc')
+
 
 //Event Listeners
 searchBtn.addEventListener('click', getWeatherDetails);
 
-function getWeatherDetails(e) {
-    e.preventDefault()
+function getWeatherDetails() {
     let searchInputTxt = document.getElementById('search-input').value;
-    let temperature = document.getElementById('weather-temp');
-    let city = document.getElementById('location')
-    let ions = document.getElementById('weather-icon')
-    let dayday = document.getElementById('date-name')
-    let cweather = document.getElementById('weather-desc')
+   
     fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchInputTxt}?unitGroup=metric&key=G3W2C9R5DN23VPB2HHAG4XEHJ&contentType=json`)
     .then(response => response.json())
     .then(getWeatherDetails => {
-        let temp = getWeatherDetails.days[0].temp;
-        let date = getWeatherDetails.days[0].datetime;
-        let location = getWeatherDetails.resolvedAddress;
-        let cwea = getWeatherDetails.days[0].description;
-        let icons = getWeatherDetails.days[0].icon;
-        temperature.append(temp)
-        cweather.append(cwea)
-        dayday.append(date)
-        city.append(location)
-        ions.append(icons)
+        // let temp = getWeatherDetails.days[0].temp;
+        // let date = getWeatherDetails.days[0].datetime;
+        // let location = getWeatherDetails.resolvedAddress;
+        // let cwea = getWeatherDetails.days[0].description;
+        // let icons = getWeatherDetails.days[0].icon;
+        // temperature.append(temp)
+        // cweather.append(cwea)
+        // dayday.append(date)
+        // city.append(location)
+        // ions.append(icons)
 
-        if (icon = partly-cloudy-day) {
-            
-        }
+        city.textContent = getWeatherDetails.resolvedAddress
+        ions.textContent = getWeatherDetails.days[0].icon
+        temperature.textContent = `${getWeatherDetails.days[0].temp}°C`
+        dayday.textContent = getWeatherDetails.days[0].datetime
+        cweather.textContent = getWeatherDetails.days[0].description
+
 
     })
-    
+        searchInputTxt.value = '';   
+}
+
+function displayBG(icon) {
+
 }
